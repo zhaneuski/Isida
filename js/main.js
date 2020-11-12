fd4bf514efb09e1d3d4385f47bc014569b076182
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-11-12 23:53:32 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,109 +1,61 @@
-//
-// const key01 = document.getElementById('01');
-// const key02 = document.getElementById('02');
-// const key03 = document.getElementById('03');
-// const key004 = document.getElementById('004');
-// const sitCenter = document.getElementById('sit_center');
-// const call = document.getElementById('call');
-// const video = document.getElementById('video');
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['!','1'].includes(event.key) ) {
-//         if (key01.hasAttribute('checked')){
-//             key01.removeAttribute('checked');
-//         } else {
-//             key01.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['@','2'].includes(event.key) ) {
-//         if (key02.hasAttribute('checked')){
-//             key02.removeAttribute('checked');
-//         } else {
-//             key02.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['#','3'].includes(event.key) ) {
-//         if (key03.hasAttribute('checked')){
-//             key03.removeAttribute('checked');
-//         } else {
-//             key03.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['$','4'].includes(event.key) ) {
-//         if (key004.hasAttribute('checked')){
-//             key004.removeAttribute('checked');
-//         } else {
-//             key004.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['%','5'].includes(event.key) ) {
-//         if (sitCenter.hasAttribute('checked')){
-//             sitCenter.removeAttribute('checked');
-//         } else {
-//             sitCenter.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['^','6'].includes(event.key) ) {
-//         if (call.hasAttribute('checked')){
-//             call.removeAttribute('checked');
-//         } else {
-//             call.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
-//
-// document.addEventListener('keydown', function(event){
-//     if (event.altKey && ['&','7'].includes(event.key) ) {
-//         if (video.hasAttribute('checked')){
-//             video.removeAttribute('checked');
-//         } else {
-//             video.setAttribute('checked', 'checked');
-//         }
-//     }
-// });
+//реализация горячих клавиш
 
+for (let i = 1; i <= 7; i++) {
+    let check = document.querySelector(`.checkbox[id = "${i}"]`);
+    document.addEventListener('keydown', function (event) {
+        if (event.altKey && i == event.key) {
+            if (check.hasAttribute('checked')) {
 
-// const description = document.getElementById('description');
-//
-//
-//
-// description.addEventListener('keydown', function (event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault();
-//         // console.log('hello');
-//     }
-//     if (event.shiftKey && event.key === "Enter") {
-//         description.value = description.value + "\n";
-//     }
-// })
+                check.removeAttribute('checked');
+            } else {
+                check.setAttribute('checked', 'checked');
+            }
+        }
+    });
+}
+
+//реализация посветки имени поля ввода
 
 const label = document.querySelectorAll('.main_label');
 const input = document.querySelectorAll('.input_text');
 
-let i;
-let j;
-
 for (i = 0; i < label.length; i++) {
+    let num = i;
     for (j = 0; j < input.length; j++) {
-        if ( i === j ) {
-            input[j].addEventListener('focus', function() {
-                label.classList.add('active');
+        if (j === num) {
+            input[j].addEventListener('focus', function () {
+                label[num].classList.add('active');
+            });
+            input[j].addEventListener('blur', function () {
+                label[num].classList.remove('active');
             });
         }
     }
 }
+
+//реализация перехода по полям ввода через Enter
+
+for (let i = 0; i < input.length - 1; i++) {
+    input[i].addEventListener('keydown', function (event) {
+        if ('Enter' == event.key || 'Tab' == event.key  ) {
+            event.preventDefault();
+        }
+
+        if (event.key === "Enter" && !event.shiftKey ) {
+            input[i+1].focus();
+        }
+    })
+}
+
+//реализация переноса строки в многострочном поле ввода
+
+const description = document.getElementById('description');
+
+description.addEventListener('keydown', function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    }
+    if (event.shiftKey && event.key === "Enter") {
+        description.value = description.value + "\n";
+    }
+})
